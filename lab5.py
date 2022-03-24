6dbe6be7b5d8c6513d11f832093a6cb0f704cb83
Vladimir Kerdiak <vladimir.kerdiak@gmail.com>
2022-03-24 16:41:18 +0200

    add Exceptions

--- lab5.py
+++ lab5.py
@@ -1,3 +1,5 @@
+import pytest
+
 from subjects.prime_num_generator import prime_num_generator
 from subjects.validator_ip import validate_ip
 from subjects.palindrom import palindrom
@@ -17,22 +19,36 @@ def test_prime_num_generator():
         if i is index - 1:
             assert v == 547
             break
-    assert isinstance(next(subject_one),int) == True
+    assert isinstance(next(subject_one), int) == True
 
 
 def test_validator_ip():
     faker = Faker()
     ip_addr = faker.ipv4()
+
+
     for i in range(50):
         assert validate_ip(ip_addr) == True
-    assert False == validate_ip("")
+
+    with pytest.raises(Exception):
+        assert False == validate_ip("1.0.0.")
+    with pytest.raises(Exception):
+        assert False == validate_ip(123)
+
     assert isinstance(validate_ip(faker.ipv4()), bool) == True
 
 
 def test_palindrom():
-    subject = "Hello, kayak, phrase!adress. mom ,!,wow ,deed ,,mom " \
+    subject = "Hello, kayak, phrase!address. mom ,!,wow ,deed ,,mom " \
               "repaper level,уму,йой,зараз,файно,радар,!дід,пів,кугут,що "
     palindrom_arr = ["kayak", "mom", "wow", "deed", "mom", "repaper", "level", "уму", "йой", "зараз", "радар", "дід"]
+
     assert palindrom_arr == palindrom(subject)
-    assert [] == palindrom("")
-    assert isinstance(palindrom(""), list)
+
+    with pytest.raises(Exception):
+        assert [] == palindrom("11")
+
+    with pytest.raises(Exception):
+            assert [] == palindrom(123)
+
+    assert isinstance(palindrom(subject), list)
--- subjects/palindrom.py
+++ subjects/palindrom.py
@@ -1,9 +1,13 @@
 import re
 
 
-def palindrom(string):
+def palindrom(input):
+    if len(input) < 3:
+        raise Exception("The argument must contain a string of more than 3 characters")
+    if not isinstance(input, str):
+        raise Exception("The argument must contain the string ")
     result = []
-    array = re.findall(r"[\w']+", string)
+    array = re.findall(r"[\w']+", input)
     for value in array:
         if len(value) > 1:
             if len(value) > 1:
